77e373b2b49639898cf83562970e2c4c289d02bf
Philip Vegdahl <pvegdahl@gmail.com>
2023-11-26 15:09:00 -0500

    Add a real @shortdoc string

--- lib/mix/tasks/advent.ex
+++ lib/mix/tasks/advent.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.Advent do
   @moduledoc "A custom mix task to run code for a specific day (Totally unnecessary)"
   use Mix.Task
 
-  @shortdoc "For now, just outputs a test string"
+  @shortdoc "Run the AoC code for the given days"
   def run(days) do
     Enum.each(days, &run_and_print_day/1)
   end
