de580a89c46060112230d9572a35654ba26b302a
Philip Vegdahl <pvegdahl@gmail.com>
2023-12-02 11:22:09 -0500

    [Day02] Set up my files

---
+++ lib/day02.ex
@@ -0,0 +1,17 @@
+defmodule AdventOfCode2023.Day02 do
+  def part_a(lines) do
+  end
+
+  def part_b(lines) do
+  end
+
+  def a() do
+    file_to_lines("inputs/day02.txt")
+    |> part_a()
+  end
+
+  def b() do
+    file_to_lines("inputs/day02.txt")
+    |> part_b()
+  end
+end
---
+++ test/day02_test.exs
@@ -0,0 +1,5 @@
+defmodule AdventOfCode2023.Day02Test do
+  use ExUnit.Case, async: true
+
+  alias AdventOfCode2023.Day02
+end
