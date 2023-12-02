7232e54bec98e7524769fef8aa90a9da56f5dacb
Philip Vegdahl <pvegdahl@gmail.com>
2023-12-02 11:45:31 -0500

    [REFACTOR] Give ColorCubes its own test module

--- test/day02_test.exs
+++ test/day02_test.exs
@@ -1,12 +1,17 @@
+
 defmodule AdventOfCode2023.Day02Test do
   use ExUnit.Case, async: true
 
   alias AdventOfCode2023.Day02
+end
+
+
+defmodule AdventOfCode2023.ColorCubesTest do
+  use ExUnit.Case, async: true
+
   alias AdventOfCode2023.ColorCubes
 
-  describe "Color Cubes" do
-    test "Parse one color of cubes from text" do
-      assert ColorCubes.from_text("3 red") == %ColorCubes{red: 3}
-    end
+  test "Parse one color of cubes from text" do
+    assert ColorCubes.from_text("3 red") == %ColorCubes{red: 3}
   end
 end
