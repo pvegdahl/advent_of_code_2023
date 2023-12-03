2996bd819b973562dbdcd64b43f8589cd51437bd
Philip Vegdahl <pvegdahl@gmail.com>
2023-12-03 06:08:05 -0500

    [Day03] Create files from templates

---
+++ lib/day03.ex
@@ -0,0 +1,19 @@
+defmodule AdventOfCode2023.Day03 do
+  alias AdventOfCode2023.Helpers
+
+  def part_a(_lines) do
+  end
+
+  def part_b(_lines) do
+  end
+
+  def a() do
+    Helpers.file_to_lines!("inputs/day03.txt")
+    |> part_a()
+  end
+
+  def b() do
+    Helpers.file_to_lines!("inputs/day03.txt")
+    |> part_b()
+  end
+end
---
+++ test/day03_test.exs
@@ -0,0 +1,9 @@
+defmodule AdventOfCode2023.Day03Test do
+  use ExUnit.Case, async: true
+
+  alias AdventOfCode2023.Day03
+
+  test "Placeholder" do
+    assert Day03.part_a("") == nil
+  end
+end
