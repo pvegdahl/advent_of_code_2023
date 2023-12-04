4a7b874d2846fef8cab868564b6dcdcc7756e048
Philip Vegdahl <pvegdahl@gmail.com>
2023-12-04 09:06:51 -0500

    [Day04] Count the matches and return a score

--- lib/day04.ex
+++ lib/day04.ex
@@ -1,7 +1,24 @@
 defmodule AdventOfCode2023.Day04 do
   alias AdventOfCode2023.Helpers
 
-  def part_a(_lines) do
+  def part_a(lines) do
+    lines
+    |> Enum.map(&score_line/1)
+    |> Enum.sum()
+  end
+
+  def score_line(line) do
+    match_count =
+      line
+      |> parse_line()
+      |> Enum.map(&MapSet.new/1)
+      |> Enum.reduce(&MapSet.intersection/2)
+      |> MapSet.size()
+
+      case match_count do
+        0 -> 0
+        pos_number -> Integer.pow(2, pos_number-1)
+      end
   end
 
   def parse_line(line) do
