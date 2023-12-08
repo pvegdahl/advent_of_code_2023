6dd2f3dbc0e2352e5e658cee1ac670af13b3fee0
Philip Vegdahl <pvegdahl@gmail.com>
2023-12-08 06:06:11 -0500

    [Day08] Add part A examples

--- test/day08_test.exs
+++ test/day08_test.exs
@@ -4,18 +4,46 @@ defmodule AdventOfCode2023.Day08Test do
   alias AdventOfCode2023.Day08
 
   @tag :skip
-  test "Day08 part A example" do
-    assert Day08.part_a(example_input()) == :something
+  test "Day08 part A example 1" do
+    assert Day08.part_a(example_input_1()) == 2
   end
 
-  defp example_input() do
+  @tag :skip
+  test "Day08 part A example 2" do
+    assert Day08.part_a(example_input_2()) == 6
+  end
+
+  defp example_input_1() do
     [
-      "TODO"
+      "RL",
+      "",
+      "AAA = (BBB, CCC)",
+      "BBB = (DDD, EEE)",
+      "CCC = (ZZZ, GGG)",
+      "DDD = (DDD, DDD)",
+      "EEE = (EEE, EEE)",
+      "GGG = (GGG, GGG)",
+      "ZZZ = (ZZZ, ZZZ)"
+    ]
+  end
+
+  defp example_input_2() do
+    [
+      "LLR",
+      "",
+      "AAA = (BBB, BBB)",
+      "BBB = (AAA, ZZZ)",
+      "ZZZ = (ZZZ, ZZZ)"
     ]
   end
 
   @tag :skip
-  test "Day08 part B example" do
-    assert Day08.part_b(example_input()) == :something_else
+  test "Day08 part B example 1" do
+    assert Day08.part_b(example_input_1()) == :something_else
+  end
+
+  @tag :skip
+  test "Day08 part B example 2" do
+    assert Day08.part_b(example_input_2()) == :something_else
   end
 end
