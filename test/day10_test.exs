74e050275ae0e5fb3946fccb64041d45604d86d9
Philip Vegdahl <pvegdahl@gmail.com>
2023-12-10 14:43:33 -0500

    [Day10] Parse input.
    Also refactor from example_input() to @example_input.
    
    My bad to not make these two commits.  I can live with this
    for AoC.

--- test/day10_test.exs
+++ test/day10_test.exs
@@ -3,48 +3,44 @@ defmodule AdventOfCode2023.Day10Test do
 
   alias AdventOfCode2023.Day10
 
+  @example_input_1 [
+    ".....",
+    ".S-7.",
+    ".|.|.",
+    ".L-J.",
+    "....."
+  ]
+
+  @example_input_2 [
+    "..F7.",
+    ".FJ|.",
+    "SJ.L7",
+    "|F--J",
+    "LJ..."
+  ]
+
   @tag :skip
   test "Day10 part A example 1" do
-    assert Day10.part_a(example_input_1()) == 4
-  end
-
-  defp example_input_1() do
-    [
-      ".....",
-      ".S-7.",
-      ".|.|.",
-      ".L-J.",
-      "....."
-    ]
+    assert Day10.part_a(@example_input_1) == 4
   end
 
   @tag :skip
   test "Day10 part A example 2" do
-    assert Day10.part_a(example_input_2()) == 8
-  end
-
-  defp example_input_2() do
-    [
-      "..F7.",
-      ".FJ|.",
-      "SJ.L7",
-      "|F--J",
-      "LJ..."
-    ]
+    assert Day10.part_a(@example_input_2) == 8
   end
 
   @tag :skip
   test "Day10 part B example" do
-    assert Day10.part_b(example_input_1()) == :something_else
+    assert Day10.part_b(@example_input_1) == :something_else
   end
 
   test "parse example 1 input" do
-    assert Day10.parse_input(example_input_1()) == {
+    assert Day10.parse_input(@example_input_1) == {
              {".", ".", ".", ".", "."},
              {".", "S", "-", "7", "."},
              {".", "|", ".", "|", "."},
              {".", "L", "-", "J", "."},
-             {".", ".", ".", ".", "."},
+             {".", ".", ".", ".", "."}
            }
   end
 end
