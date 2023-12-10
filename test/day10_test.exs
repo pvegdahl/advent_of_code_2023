445101e32c18d972d73120b1f61cd4cefd638ff1
Philip Vegdahl <pvegdahl@gmail.com>
2023-12-10 14:37:08 -0500

    [Day10] Add tests for part A examples

--- test/day10_test.exs
+++ test/day10_test.exs
@@ -4,18 +4,37 @@ defmodule AdventOfCode2023.Day10Test do
   alias AdventOfCode2023.Day10
 
   @tag :skip
-  test "Day10 part A example" do
-    assert Day10.part_a(example_input()) == :something
+  test "Day10 part A example 1" do
+    assert Day10.part_a(example_input_1()) == 4
   end
 
-  defp example_input() do
+  defp example_input_1() do
     [
-      "TODO"
+      ".....",
+      ".S-7.",
+      ".|.|.",
+      ".L-J.",
+      "....."
+    ]
+  end
+
+  @tag :skip
+  test "Day10 part A example 2" do
+    assert Day10.part_a(example_input_2()) == 8
+  end
+
+  defp example_input_2() do
+    [
+      "..F7.",
+      ".FJ|.",
+      "SJ.L7",
+      "|F--J",
+      "LJ..."
     ]
   end
 
   @tag :skip
   test "Day10 part B example" do
-    assert Day10.part_b(example_input()) == :something_else
+    assert Day10.part_b(example_input_1()) == :something_else
   end
 end
