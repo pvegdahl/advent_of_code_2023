896628621594b809d56c05405695644c7774e77b
Philip Vegdahl <pvegdahl@gmail.com>
2023-12-11 06:59:10 -0500

    mix format

--- lib/day10.ex
+++ lib/day10.ex
@@ -101,7 +101,9 @@ defmodule AdventOfCode2023.Day10 do
   def enclosed_area_of_line(pipe_map, loop, line_num) do
     loop_points_on_this_line = Enum.filter(loop, fn {_x, y} -> y == line_num end)
 
-    Enum.reduce(loop_points_on_this_line, {0, nil, :outside}, fn point, acc -> enclosed_area_reduce_helper(pipe_map, point, acc) end)
+    Enum.reduce(loop_points_on_this_line, {0, nil, :outside}, fn point, acc ->
+      enclosed_area_reduce_helper(pipe_map, point, acc)
+    end)
     |> elem(0)
   end
 
@@ -114,7 +116,8 @@ defmodule AdventOfCode2023.Day10 do
 
   defp enclosed_area_reduce_helper(pipe_map, point, {count, last_relevant_point, :outside}) do
     last_type = get(pipe_map, last_relevant_point)
-    this_type =  get(pipe_map, point)
+    this_type = get(pipe_map, point)
+
     case {last_type, this_type} do
       {"|", "|"} -> {count, point, :inside}
       {"7", "|"} -> {count, point, :inside}
@@ -126,7 +129,8 @@ defmodule AdventOfCode2023.Day10 do
 
   defp enclosed_area_reduce_helper(pipe_map, point, {count, last_relevant_point, :inside}) do
     last_type = get(pipe_map, last_relevant_point)
-    this_type =  get(pipe_map, point)
+    this_type = get(pipe_map, point)
+
     case {last_type, this_type} do
       {"|", "|"} -> {count + inside_count(last_relevant_point, point), point, :outside}
       {"7", "|"} -> {count + inside_count(last_relevant_point, point), point, :outside}
