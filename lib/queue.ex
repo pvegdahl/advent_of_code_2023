f4b2254187a15173de1e64875ce6e24ed7b69f9f
Philip Vegdahl <pvegdahl@gmail.com>
2023-12-21 15:05:52 -0500

    [REFACTOR] Use elements to determine emptiness of queue

--- lib/queue.ex
+++ lib/queue.ex
@@ -1,11 +1,12 @@
 defmodule AdventOfCode2023.Queue do
-  defstruct is_empty: true, elements: []
+  defstruct elements: []
 
   def new(), do: %__MODULE__{}
 
-  def empty?(%__MODULE__{is_empty: is_empty}), do: is_empty
+  def empty?(%__MODULE__{elements: []}), do: true
+  def empty?(%__MODULE__{elements: _}), do: false
 
-  def push(%__MODULE__{elements: elements}, element), do: %__MODULE__{is_empty: false, elements: [element | elements]}
+  def push(%__MODULE__{elements: elements}, element), do: %__MODULE__{elements: [element | elements]}
 
   def pop(%__MODULE__{elements: elements}) do
     {remaining_elements, [front]} = Enum.split(elements, -1)
