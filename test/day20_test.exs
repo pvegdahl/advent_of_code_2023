6594bb2f6302bfecf03ae6bf0ab8ad3a73109e42
Philip Vegdahl <pvegdahl@gmail.com>
2023-12-21 14:39:56 -0500

    [Day20] Add AoC examples as unit tests

--- test/day20_test.exs
+++ test/day20_test.exs
@@ -3,19 +3,34 @@ defmodule AdventOfCode2023.Day20Test do
 
   alias AdventOfCode2023.Day20
 
+  @example_input_1 [
+    "broadcaster -> a, b, c",
+    "%a -> b",
+    "%b -> c",
+    "%c -> inv",
+    "&inv -> a"
+  ]
+
   @tag :skip
-  test "Day20 part A example" do
-    assert Day20.part_a(example_input()) == :something
+  test "Day20 part A example 1" do
+    assert Day20.part_a(@example_input_1) == 32_000_000
   end
 
-  defp example_input() do
-    [
-      "TODO"
-    ]
+  @example_input_2 [
+    "broadcaster -> a",
+    "%a -> inv, con",
+    "&inv -> b",
+    "%b -> con",
+    "&con -> output"
+  ]
+
+  @tag :skip
+  test "Day20 part A example 2" do
+    assert Day20.part_a(@example_input_2) == 11_687_500
   end
 
   @tag :skip
   test "Day20 part B example" do
-    assert Day20.part_b(example_input()) == :something_else
+    assert Day20.part_b(@example_input_1) == :something_else
   end
 end
