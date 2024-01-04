b7643c19df6f2c8fa18a5858a605e0abe48cbecb
Philip Vegdahl <pvegdahl@gmail.com>
2024-01-04 11:22:57 -0500

    Add a more complex test for map of lists reversal

--- test/helpers_test.exs
+++ test/helpers_test.exs
@@ -10,4 +10,14 @@ defmodule AdventOfCode2023.HelpersTest do
   test "A reversed map of lists with two keys, each with one distinct value" do
     assert Helpers.reverse_map_of_lists(%{1 => [2], 3 => [4]}) == %{2 => [1], 4 => [3]}
   end
+
+  test "A complex test of reversed map of lists" do
+    assert Helpers.reverse_map_of_lists(%{1 => [2, 3, 4], 0 => [4, 5, 6]}) == %{
+             2 => [1],
+             3 => [1],
+             4 => [0, 1],  # Note: This ordering is only guaranteed by the current Erlang implementation of small maps.
+             5 => [0],
+             6 => [0]
+           }
+  end
 end
